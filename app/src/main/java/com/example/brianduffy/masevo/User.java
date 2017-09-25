c5546bac3cbfaad9c2ced688f8d24557bb8eeff8
GKiddy <gkiddy@purdue.edu>
2017-09-24 20:32:20 -0400

    Added items to User Class

--- app/src/main/java/com/example/brianduffy/masevo/User.java
+++ app/src/main/java/com/example/brianduffy/masevo/User.java
@@ -1,8 +1,36 @@
 package com.example.brianduffy.masevo;
 
+import java.util.List;
+
 /**
  * Created by Brian Duffy on 9/14/2017.
  */
 
 public class User {
+    String username;
+    String emailAddress;
+    List myEventList;
+    Region myRegion;
+    Location myLocation;
+    List nearbyRegions;
+
+    // Gets and sets a users Location
+    public void getMyLocation()
+    //public Location getMyLocation()
+    {
+
+    }
+
+    // Allows a user to attempt to join a given Event
+    public void joinEvent(Event eventToJoin)
+    {
+
+    }
+
+    // Gets the Regions surrounding a given user
+    public void getNearRegions()
+    //public List<Region> getNearRegions()
+    {
+
+    }
 }
