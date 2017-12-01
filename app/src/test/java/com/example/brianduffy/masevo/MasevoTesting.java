6227b6ec2c340b8064edfce22269b09f570f4226
vk117 <vpasuma@purdue.edu>
2017-12-01 00:47:38 -0500

    First test case for creating and deleting event

--- app/src/test/java/com/example/brianduffy/masevo/MasevoTesting.java
+++ app/src/test/java/com/example/brianduffy/masevo/MasevoTesting.java
@@ -1,9 +1,18 @@
 package com.example.brianduffy.masevo;
 
+import android.util.Pair;
+
+import com.example.chambe41.masevo.ThreadCreateEvent;
+import com.example.chambe41.masevo.ThreadDeleteEvent;
+
+import junit.framework.Assert;
+
 import org.junit.Test;
 
+import java.util.Calendar;
+
 /**
-* Masevo Unit Testing
+* Masevo Backend Unit Testing
 *
 * @author Vikram Pasumarti, vpasuma@purdue.edu
 * @version 30 November 2017
@@ -24,9 +33,36 @@ ERRNO
 */
 
 public class MasevoTesting {
-
+    Calendar temp = Calendar.getInstance();
     @Test
-    public void createEventServerTest() {
+    public void createEventDeleteEventServerTest() throws InterruptedException {
+        String eventName = "Sample Event";
+        String eventDesc = "This is an Event Description";
+        java.sql.Date startDate = new java.sql.Date(1543622400000l);
+        java.sql.Date endDate = new java.sql.Date(1543626000000l);
+        Location location = new Location(0,0);
+        float radius = 10;
+        int eventId = 0;
+        String hostEmail = "testing.masevo";
+        boolean pub = true;
+        ThreadCreateEvent threadCreate = new ThreadCreateEvent(eventName, eventDesc, startDate, endDate, location, radius, eventId, hostEmail, pub);
+        Thread createEventThread = new Thread(threadCreate);
+        createEventThread.start();
+        PublicEvent publicEvent = new PublicEvent(eventName,eventDesc,startDate,endDate,location.latitude,location.longitude,radius,hostEmail);
+        Pair<PublicEvent, Integer> expected = new Pair<>(publicEvent,0);
+
+        createEventThread.join();
+        Pair<Event,Integer> actual = threadCreate.getReturnResult();
+
+        Assert.assertEquals(expected.second,actual.second); //checks if event made successfully
+
+        ThreadDeleteEvent threadDelete = new ThreadDeleteEvent(0,1);
+        Thread deleteEventThread = new Thread(threadDelete);
+        deleteEventThread.start();
+        deleteEventThread.join();
+        Pair<Boolean, Integer> expectedDel = threadDelete.getReturnResult();
+
+        Assert.assertEquals((Integer) 0,expectedDel.second); //checks if event deleted successfully
 
     }
 
@@ -37,11 +73,6 @@ public class MasevoTesting {
 
     @Test
     public void modifyEventServerTest() {
-
-    }
-
-    @Test
-    public void addUserServerTest() {
 
     }
 
