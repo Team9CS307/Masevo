af6eab93144f742a5cce27a6f7a0db75450a65a9
vk117 <vpasuma@purdue.edu>
2017-12-01 13:19:26 -0500

    Added test for modifying event success and modifying event permission error

--- app/src/test/java/com/example/brianduffy/masevo/MasevoTesting.java
+++ app/src/test/java/com/example/brianduffy/masevo/MasevoTesting.java
@@ -5,11 +5,13 @@ import android.util.Pair;
 import com.example.chambe41.masevo.ThreadCreateEvent;
 import com.example.chambe41.masevo.ThreadDeleteEvent;
 import com.example.chambe41.masevo.ThreadJoinEvent;
+import com.example.chambe41.masevo.ThreadModifyEvent;
 
 import junit.framework.Assert;
 
 import org.junit.Test;
 
+import java.sql.Date;
 import java.util.Calendar;
 
 /**
@@ -101,7 +103,43 @@ public class MasevoTesting {
     }
 
     @Test
-    public void modifyEventServerTest() {
+    public void modifyEventServerTest() throws InterruptedException {
+        String eventName = "Sample Event";
+        String eventDesc = "This is an Event Description";
+        java.sql.Date startDate = new java.sql.Date(1543622400000l);
+        java.sql.Date endDate = new java.sql.Date(1543626000000l);
+        Location location = new Location(0,0);
+        float radius = 10;
+        int eventId = 0;
+        String hostEmail = "testing.masevo";
+        boolean pub = true;
+        ThreadCreateEvent threadCreate = new ThreadCreateEvent(eventName, eventDesc, startDate, endDate, location, radius, eventId, hostEmail, pub);
+        Thread createEventThread = new Thread(threadCreate);
+        createEventThread.start();
+        PublicEvent publicEvent = new PublicEvent(eventName,eventDesc,startDate,endDate,location.latitude,location.longitude,radius,hostEmail);
+        Pair<PublicEvent, Integer> expected = new Pair<>(publicEvent,0);
+        createEventThread.join();
+
+        ThreadModifyEvent threadModifyEvent = new ThreadModifyEvent(eventId, eventName, eventDesc, startDate, endDate, location.latitude, location.longitude, radius, hostEmail, pub);
+        Thread modifyEventThread = new Thread(threadModifyEvent);
+        modifyEventThread.start();
+        modifyEventThread.join();
+        Pair<Event, Integer> actual = threadModifyEvent.getReturnResult();
+
+        Assert.assertEquals((Integer) 0, actual.second); //checks if event is successfully modified
+
+        ThreadJoinEvent threadJoinEvent = new ThreadJoinEvent(0,"user", true);
+        Thread joinEventThread = new Thread(threadJoinEvent);
+        joinEventThread.start();
+        joinEventThread.join();
+
+        ThreadModifyEvent threadModifyEvent2 = new ThreadModifyEvent(eventId, eventName, eventDesc, startDate, endDate, location.latitude, location.longitude, radius, "user", pub);
+        Thread modifyEventThread2 = new Thread(threadModifyEvent2);
+        modifyEventThread2.start();
+        modifyEventThread2.join();
+        Pair<Event, Integer> actualFail = threadModifyEvent.getReturnResult();
+
+        Assert.assertEquals((Integer) 2, actualFail.second); //checks if permissions error is thrown because normal users should not be able to modify events.
 
     }
 
